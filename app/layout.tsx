1721d763f581479750993105a8af50c4b9bad841
Fathul Mu'min <fathulmumin76040@gmail.com>
2026-08-10 14:03:47 +0800

    fix: ganti favicon dari .ico ke .png

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export const metadata: Metadata = {
   title: "AbsenKi' - Absensi Digital yang Mudah, Cepat, dan Akurat",
   description: "Aplikasi Absensi Digital berbasis QR Code",
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
     apple: '/apple-touch-icon.png',
   },
 };
