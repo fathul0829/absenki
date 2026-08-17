8dc06839d9fff83db5a764ca667dfd0d34444571
Fathul Mu'min <fathulmumin76040@gmail.com>
2026-08-17 14:24:52 +0800

    style: update footer logo to logo side text (w bg)

--- app/page.tsx
+++ app/page.tsx
@@ -528,7 +528,7 @@ export default function LandingPage() {
             <div className="flex flex-col items-center md:items-start">
               <div className="relative h-8 w-36 mb-2">
                 <Image
-                  src="absenki/public/logo/logo side text (w bg).png"
+                  src="/logo/logo side text (w bg).png"
                   alt="AbsenKi Logo"
                   fill
                   className="object-contain object-left"
